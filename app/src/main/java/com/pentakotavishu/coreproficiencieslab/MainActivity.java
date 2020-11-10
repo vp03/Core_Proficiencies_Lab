e9d49ea9ad8af8a440b6bdfc914e1ef9415afee1
vishpentak <vishpentak@gmail.com>
2020-11-10 14:23:16 -0500

    Initial commit

---
+++ app/src/main/java/com/pentakotavishu/coreproficiencieslab/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pentakotavishu.coreproficiencieslab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckboxClicked(View view) {
+    }
+
+    public void submit_vote(View view) {
+    }
+}
